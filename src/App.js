8a0f2e0da2cd8b9abc72bb06b879dc670a3672c9
Noureddine ABABAR <noureddine.ababar@gmail.com>
2022-11-15 15:36:20 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [utilisateurs, setUtilisateurs] = useState([])
+  const [articles, setArticles] = useState([])
+  const [userActif, setUserActif] = useState(0)
+  useEffect(()=> {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response)=>{return response.json()})
+    .then((data)=>setUtilisateurs(data));
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response)=>{return response.json()})
+    .then((data)=>setArticles(data));
+  }, [])
+  function handleClick(id) {
+    setUserActif(id)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding:"20px"}}>
+      <h1>Liste de utilisateurs</h1>
+      {
+        utilisateurs.map((utilisateur, index) => {
+          return (
+            <p key={index}>
+              {utilisateur.name} 
+              <button onClick={()=>handleClick(utilisateur.id)}>détails des articles</button>
+              <div>
+                {
+                  userActif===utilisateur.id
+                  ?<ul> {
+                    articles.filter((item)=>item.userId===parseInt(userActif))
+                    .map((article)=> {
+                      return <li>{article.id} {article.title}</li>
+                    })
+                    }
+                    </ul>
+                  :null
+                }
+              </div>
+            </p>
+          )
+        })
+      }
     </div>
   );
 }
